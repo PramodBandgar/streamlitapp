11b2b9e275729b00cec688f94c4f7001f7eeb93d
Pramod Jagannath Bandgar <91980825+PramodBandgar@users.noreply.github.com>
2023-05-03 14:28:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -56,10 +56,10 @@ if (selected == 'Diabetes Prediction'):
          Insulin = st.number_input('Insulin Level', min_value=0, max_value=1000, format='%d')
 
     with col1:
-         BMI = st.number_input('BMI value', min_value=0, max_value=50, format='%i')
+         BMI = st.number_input('BMI value', min_value=0.0, max_value=50.0, format='%i')
 
     with col2:
-         DiabetesPedigreeFunction = st.number_input('Diabetes Pedigree Function value', min_value=0, max_value=3, format='%i')
+         DiabetesPedigreeFunction = st.number_input('Diabetes Pedigree Function value', min_value=0.000, max_value=3.000, format='%i')
 
     with col3:
          Age = st.number_input('Age of the Person', min_value=1, max_value=120, format='%d')
